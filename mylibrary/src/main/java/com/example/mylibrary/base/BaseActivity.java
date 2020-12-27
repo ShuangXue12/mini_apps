bb5171a7fd6f28b6675ab2dd8cd9a00f92fef2ae
‘ShuangXue12’ <you@example.com>
2020-12-27 23:08:50 +0800

    提交第五次

--- mylibrary/src/main/java/com/example/mylibrary/base/BaseActivity.java
+++ mylibrary/src/main/java/com/example/mylibrary/base/BaseActivity.java
@@ -6,7 +6,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity <p extends BasePresenter> extends AppCompatActivity implements BaseView{
-    public   p presenter;
+    public p presenter;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
